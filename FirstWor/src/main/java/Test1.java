50098bbb1340e009ce37713037e938075570870c
vladyslav ocheret <vladyslav.ocheret@gmail.com>
2021-11-02 12:01:34 +0200

    4

--- FirstWor/src/main/java/Test1.java
+++ FirstWor/src/main/java/Test1.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -21,12 +22,15 @@ public class Test1 {
         driver.get(c);
         driver.findElement(xpath).sendKeys(s, Keys.ENTER);
         Thread.sleep(2000);
-        WebDriverWait webDriverWait=new WebDriverWait(driver, Duration.ofSeconds(10));
-        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//title")));
 
-        driver.close();
 
     }
+    public void read(){
+        WebDriverWait webDriverWait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a//h3")));
+
+    }
+
 
 
 }
--- FirstWor/src/test/java/Test2.java
+++ FirstWor/src/test/java/Test2.java
@@ -2,22 +2,37 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Test2  {
-
+String s="https://www.google.com/";
     private Test1 test1;
+    @BeforeTest
+    public void beforetest(){
+        System.setProperty("webdriver.chrome.driver","C:\\tools\\ChromeDriver\\chromedriver.exe");
+        test1=new Test1(new ChromeDriver(),s);
+        System.out.println("Before");
+    }
+    @AfterTest
+    public void after(){
+        test1.read();
+        System.out.println("after tets");
+    }
     @Test
     public void setTest1(){
-        System.setProperty("webdriver.chrome.driver","C:\\tools\\ChromeDriver\\chromedriver.exe");
-         test1=new Test1(new ChromeDriver(),"https://www.google.com/");
+
          try {
              test1.write("java");
+             System.out.println("test");
 
     }catch (Exception e){
              e.printStackTrace();
          }
     }
+
+
 
 
 }
